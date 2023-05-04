33d7f5a64abf30e0b4bf437b30b6ea64bcc6bea1
kira924age <kira924age@gmail.com>
2023-05-04 15:02:37 +0900

    add lint-staged.config.js

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+// lint-staged.config.js
+export default {
+  '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+  'src/**/*.{js,jsx,ts,tsx}': 'eslint --fix --no-ignore --max-warnings=0',
+  'src/**/*.{css,scss}': 'stylelint --fix',
+  'src/**/*': 'prettier --write --ignore-unknown'
+}
